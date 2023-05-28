223cd9390941bfdc8b253f5d6da7703e877a1f2b
Lara Santos Borges <104030282+larasborges@users.noreply.github.com>
2023-05-28 12:27:31 -0300

    criação
    Criação de telas consultar agendamentos, remarcar consulta, consulta emergencia e modificação tela ajuda

--- lib/tela_consulta_emergencia.dart
+++ lib/tela_consulta_emergencia.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ConsultaEmergencia extends StatelessWidget {
+  final String _phoneNumber = '5534988785945';
+
+  Future<void> _launchUrl() async {
+    final String url = 'https://wa.me/$_phoneNumber';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw Exception('Não foi possível executar o $url');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 150, 57, 213),
-        actions: [],
         leading: IconButton(
           icon: Icon(
             Icons.chevron_left,
@@ -16,7 +27,38 @@ class ConsultaEmergencia extends StatelessWidget {
           onPressed: () => Navigator.pop(context, false),
         ),
       ),
-      body: Container(),
+      body: Container(
+        child: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 200,
+                    height: 250,
+                    child: Image.asset("assets/wpp.png"),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    "Para redirecionarmos você para nosso WhatsApp de emergência, toque no botão acima.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+            Positioned.fill(
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: _launchUrl,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/tela_consultar_agendamentos.dart
+++ lib/tela_consultar_agendamentos.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 
 class ConsultarAgendamentos extends StatelessWidget {
+  final List<Agendamento> agendamentos = [
+    Agendamento(
+      paciente: 'João da Silva',
+      data: '25/05/2023',
+      hora: '14:30',
+      medico: 'Dr. João',
+    ),
+    Agendamento(
+      paciente: 'Maria Oliveira',
+      data: '27/05/2023',
+      hora: '09:00',
+      medico: 'Dr. Maria',
+    ),
+    Agendamento(
+      paciente: 'Pedro Santos',
+      data: '30/05/2023',
+      hora: '16:45',
+      medico: 'Dr. Pedro',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 150, 57, 213),
-        actions: [],
         leading: IconButton(
           icon: Icon(
             Icons.chevron_left,
@@ -16,18 +36,123 @@ class ConsultarAgendamentos extends StatelessWidget {
           onPressed: () => Navigator.pop(context, false),
         ),
       ),
-      body: Container(
-        alignment: Alignment.topCenter,
-        padding: EdgeInsets.only(top: 30),
-        child: Text(
-          'Consultar Agendamentos',
-          style: TextStyle(
-            fontSize: 30,
-            color: Color.fromARGB(255, 62, 52, 169),
-            fontWeight: FontWeight.bold,
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 30),
+            child: Text(
+              'Consultar Agendamentos',
+              style: TextStyle(
+                fontSize: 30,
+                color: Color.fromARGB(255, 62, 52, 169),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-        ),
+          Divider(
+            color: Color.fromARGB(255, 150, 57, 213),
+            thickness: 1,
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: agendamentos.length,
+              itemBuilder: (context, index) {
+                final agendamento = agendamentos[index];
+                return GestureDetector(
+                  onTap: () {
+                    _mostrarDetalhes(context, agendamento);
+                  },
+                  child: Card(
+                    child: ListTile(
+                      title: Text(
+                        'Paciente: ${agendamento.paciente}',
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Data: ${agendamento.data}',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          Text(
+                            'Hora: ${agendamento.hora}',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          Text(
+                            'Médico: ${agendamento.medico}',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      trailing: Icon(Icons.remove_red_eye),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
+
+  void _mostrarDetalhes(BuildContext context, Agendamento agendamento) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Detalhes da Consulta', style: TextStyle(fontSize: 20)),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Paciente: ${agendamento.paciente}',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 15),
+              Text(
+                'Data: ${agendamento.data}',
+                style: TextStyle(fontSize: 18),
+              ),
+              Text(
+                'Hora: ${agendamento.hora}',
+                style: TextStyle(fontSize: 18),
+              ),
+              Text(
+                'Médico: ${agendamento.medico}',
+                style: TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Fechar', style: TextStyle(fontSize: 18)),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class Agendamento {
+  final String paciente;
+  final String data;
+  final String hora;
+  final String medico;
+
+  Agendamento({
+    required this.paciente,
+    required this.data,
+    required this.hora,
+    required this.medico,
+  });
 }
